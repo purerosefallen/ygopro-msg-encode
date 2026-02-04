3c6ad0d9d1d0848abf48dd188b7a3f7e0aaa8208
purerosefallen <78877@qq.com>
2026-02-04 11:24:51 +0800

    clear everything in MSG_SET

--- src/protos/msg/proto/set.ts
+++ src/protos/msg/proto/set.ts
@@ -7,4 +7,32 @@ export class YGOProMsgSet extends YGOProMsgBase {
 
   @BinaryField('i32', 0)
   code: number;
+
+  // MSG_SET 是盖放卡片，所有人都看不到 code（包括玩家自己）
+  // 服务器在发送前会将 code 清零
+  opponentView(): this {
+    const view = this.copy();
+    view.code = 0;
+    return view;
+  }
+
+  teammateView(): this {
+    const view = this.copy();
+    view.code = 0;
+    return view;
+  }
+
+  observerView(): this {
+    const view = this.copy();
+    view.code = 0;
+    return view;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  playerView(playerId: number): this {
+    // 盖放卡片，即使是玩家自己也看不到
+    const view = this.copy();
+    view.code = 0;
+    return view;
+  }
 }
