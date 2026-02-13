59bb01eeb50760db2d1ed6cfac2e3a849560d984
水濑真白 <mashiro@risayuzuki.com>
2026-02-13 18:50:12 +0800

    fix: correct MSG_MISSED_EFFECT payload layout

--- src/protos/msg/proto/missed-effect.ts
+++ src/protos/msg/proto/missed-effect.ts
@@ -5,16 +5,21 @@ import { YGOProMsgBase } from '../base';
 export class YGOProMsgMissedEffect extends YGOProMsgBase {
   static identifier = OcgcoreCommonConstants.MSG_MISSED_EFFECT;
 
-  @BinaryField('u8', 0)
-  player: number;
+  /**
+   * ocgcore (ygopro-core) sends:
+   *   - uint32: handler card info_location (controller/location/sequence/position packed)
+   *   - uint32: handler card code
+   *
+   * See ygopro-core/processor.cpp::field::break_effect()
+   */
+  @BinaryField('u32', 0)
+  location: number;
 
-  @BinaryField('i32', 1)
+  @BinaryField('u32', 4)
   code: number;
 
-  @BinaryField('i32', 5)
-  desc: number;
-
   getSendTargets(): number[] {
-    return [this.player];
+    // Core does not include a target player byte; this is a broadcast stoc message.
+    return [0, 1];
   }
 }
